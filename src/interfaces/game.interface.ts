a724a9e87af59e191a1df3e35b5be146ff207be6
Borislav Vučićević <borislavvucicevic516@gmail.com>
2025-05-25 04:00:14 +0200

    Initial commit

--- src/interfaces/game.interface.ts
+++ src/interfaces/game.interface.ts
@@ -1,14 +1,4 @@
-import type IAsocijacije from "./asocijacije.interface";
-import type IDesnoLijevo from "./desnoLijevo.interface";
-import type IMojBroj from "./mojBroj.interface";
-import type IParovi from "./parovi.interface";
-import type IPremetaljka from "./premetaljka.interface";
-import type IPutOkoSvijeta from "./putOkoSvijeta.interface";
-import type ISef from "./sef.interface";
-import type ISkriveneStaze from "./skriveneStaze.interface";
 import type ISlagalica from "./slagalica.interface";
-import type ISpajalica from "./spajalica.interface";
-import type IZid from "./zid.interface";
 
 export default interface IGame {
   slagalica: ISlagalica,
